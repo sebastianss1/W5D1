989ae37a80221f5039cb98537bf46da56ff1fcf9
Sebastian Sanchez <sebastian@artxcode.io>
2021-03-22 11:22:46 -0400

    finished MaxIntSet

--- HashMap/lib/p01_int_set.rb
+++ HashMap/lib/p01_int_set.rb
@@ -1,22 +1,35 @@
 class MaxIntSet
+
+  attr_reader :store
+
   def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true 
   end
 
   def remove(num)
+    validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num.between?(0, @max)
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
